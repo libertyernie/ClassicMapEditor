67f539efc99e414a75ec0a4d565d2bf1014f9539
libertyernie <maybeway36@gmail.com>
2018-07-24 15:21:08 -0500

    Add Wii Remote button mapping config; check code format in text box before trying to load

--- ClassicMapEditor/Form1.vb
+++ ClassicMapEditor/Form1.vb
@@ -1,4 +1,6 @@
 ﻿Imports System.IO
+Imports System.Text
+Imports System.Text.RegularExpressions
 Imports ClassicMapLib
 
 Public Class Form1
@@ -7,15 +9,25 @@ Public Class Form1
     Private Sub SetCode(code As ButtonMappingCode)
         _code = code
 
-        UpdateTextBox()
 
         FlowLayoutPanel1.Controls.Clear()
 
-        For Each mapping In _code.GetButtonMappings
-            Dim control = New MappingControl(mapping)
-            AddHandler control.ClassicControllerButtonChanged, AddressOf UpdateTextBox
-            FlowLayoutPanel1.Controls.Add(control)
-        Next
+        If _code IsNot Nothing Then
+            UpdateTextBox()
+
+            For Each mapping In _code.GetButtonMappings
+                If TypeOf mapping Is WiiRemoteButtonMapping Then
+                    Dim control = New WiiRemoteMappingControl(mapping)
+                    AddHandler control.ClassicControllerButtonChanged, AddressOf UpdateTextBox
+                    AddHandler control.WiiRemoteButtonChanged, AddressOf UpdateTextBox
+                    FlowLayoutPanel1.Controls.Add(control)
+                Else
+                    Dim control = New MappingControl(mapping)
+                    AddHandler control.ClassicControllerButtonChanged, AddressOf UpdateTextBox
+                    FlowLayoutPanel1.Controls.Add(control)
+                End If
+            Next
+        End If
     End Sub
 
     Private Sub UpdateTextBox()
@@ -29,15 +41,25 @@ Public Class Form1
         End If
     End Sub
 
+    Private GCTLine As New Regex("^[0-9A-Fa-f]{8} [0-9A-Fa-f]{8}$")
+
     Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
         If _code Is Nothing OrElse _code.ToString <> TextBox1.Text Then
             If TextBox1.Text.Contains(vbCrLf) Then
-                Dim name = TextBox1.Text.Substring(0, TextBox1.Text.IndexOf(vbCrLf)).Trim
-                Dim code = TextBox1.Text.Substring(TextBox1.Text.IndexOf(vbCrLf)).Trim
-                Dim newCode = New ButtonMappingCode(name, code)
-                If newCode.ToString.Trim = TextBox1.Text.Trim Then
-                    SetCode(newCode)
-                End If
+                Dim lines = TextBox1.Text.TrimEnd.Split(New String() {vbCrLf}, StringSplitOptions.None)
+
+                Dim name = lines.First
+                Dim code As New StringBuilder
+                For Each line In lines.Skip(1)
+                    If Not GCTLine.IsMatch(line) Then
+                        SetCode(Nothing)
+                        Exit Sub
+                    End If
+                    code.AppendLine(line)
+                Next
+
+                Dim newCode = New ButtonMappingCode(name, code.ToString)
+                SetCode(newCode)
             End If
         End If
     End Sub
--- ClassicMapEditor/WiiRemoteMappingControl.vb
+++ ClassicMapEditor/WiiRemoteMappingControl.vb
@@ -1,12 +1,38 @@
 ﻿Imports ClassicMapLib
 
 Public Class WiiRemoteMappingControl
-    Private Sub WiiRemoteMappingControl_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+    Private ReadOnly _mapping As WiiRemoteButtonMapping
+
+    Public Event ClassicControllerButtonChanged As EventHandler
+    Public Event WiiRemoteButtonChanged As EventHandler
+
+    Sub New(mapping As WiiRemoteButtonMapping)
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
         For Each value In [Enum].GetValues(GetType(ClassicControllerButton))
             ClassicControllerDropDownList.Items.Add(value)
         Next
         For Each value In [Enum].GetValues(GetType(WiiRemoteButton))
             WiiRemoteDropDownList.Items.Add(value)
         Next
+
+        _mapping = mapping
+
+        ClassicControllerDropDownList.SelectedItem = _mapping.ClassicControllerButton
+        WiiRemoteDropDownList.SelectedItem = _mapping.WiiRemoteButton
+
+    End Sub
+
+    Private Sub ClassicControllerDropDownList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ClassicControllerDropDownList.SelectedIndexChanged
+        _mapping.ClassicControllerButton = CType(ClassicControllerDropDownList.SelectedItem, ClassicControllerButton)
+        RaiseEvent ClassicControllerButtonChanged(Me, New EventArgs)
+    End Sub
+
+    Private Sub WiiRemoteDropDownList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles WiiRemoteDropDownList.SelectedIndexChanged
+        _mapping.WiiRemoteButton = CType(WiiRemoteDropDownList.SelectedItem, WiiRemoteButton)
+        RaiseEvent WiiRemoteButtonChanged(Me, New EventArgs)
     End Sub
 End Class
